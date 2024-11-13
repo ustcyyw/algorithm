cd9f60f2da136a8e4fa1659aa0bcfd13a7ea74f6
ustcyyw <yang0@mail.ustc.edu.cn>
2024-11-13 09:35:38 +0800

    2024-11-12

---
+++ leetcode/medium/greedy/leastInterval621.cpp
@@ -0,0 +1,20 @@
+/**
+ * @Time : 2024/11/12-8:56 PM
+ * @Author : yyw@ustc
+ * @E-mail : yang0@mail.ustc.edu.cn
+ * @Github : https://github.com/ustcyyw
+ * @desc : 
+ */
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    
+};
+
+int main(){
+    Solution s;
+    
+    cout << 0 << endl;
+}
