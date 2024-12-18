97494d34b8bffc1b25b2aedc1f8b32fca3d1e654
ustcyyw <yang0@mail.ustc.edu.cn>
2024-12-18 09:23:49 +0800

    2024-12-17

---
+++ luogu/graph/diameter/CF1805D.cpp
@@ -0,0 +1,20 @@
+/**
+ * @Time : 2024/12/17-9:44 PM
+ * @Author : yyw@ustc
+ * @E-mail : yang0@mail.ustc.edu.cn
+ * @Github : https://github.com/ustcyyw
+ * @desc : 
+ */
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    
+};
+
+int main(){
+    Solution s;
+    
+    cout << 0 << endl;
+}
