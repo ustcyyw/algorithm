fdd0ff5b20ba0f5930f60d8c1c72e993d17027f0
ustcyyw <yang0@mail.ustc.edu.cn>
2025-01-26 13:22:36 +0800

    2025-01-25

---
+++ luogu/brute_force/CF18A.cpp
@@ -0,0 +1,20 @@
+/**
+ * @Time : 2025/1/26-1:22 PM
+ * @Author : yyw@ustc
+ * @E-mail : yang0@mail.ustc.edu.cn
+ * @Github : https://github.com/ustcyyw
+ * @desc : 
+ */
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    
+};
+
+int main(){
+    Solution s;
+    
+    cout << 0 << endl;
+}
