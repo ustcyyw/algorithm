4b243b3e95d870cec5f6869d53344084e24657e7
ustcyyw <yang0@mail.ustc.edu.cn>
2025-04-18 14:49:03 +0800

    2025-04-17

--- README.md
+++ README.md
@@ -589,3 +589,5 @@ solve problems in leetcode, codeforce, atcoder
   * CF2094G 1752 模拟 数据结构
   * CF2094F 1597 构造算法 猜结论
   * CF2094E 1256 位运算
+* 2025-04-17
+  * CF2093G 二分答案 trie树 滑动窗口 （卡常毒瘤题）
--- test/luogu.cpp
+++ test/luogu.cpp
@@ -9,35 +9,14 @@
 using namespace std;
 typedef long long ll;
 typedef long double ld;
-const int N = 2e5 + 15, M = 30;
-int T, n, nums[N], cnt0[32], cnt1[32];
-
-ll cal(int num) {
-    ll sum = 0;
-    for(int j = 0; j < 32; j++) {
-        ll temp = 1 << j;
-        if(temp & num) sum += temp * cnt0[j];
-        else sum += temp * cnt1[j];
-    }
-    return sum;
-}
+const int N = 2e5 + 15, D = 30;
+int T, n, K, nums[N];
 
 int main() {
-    ios::sync_with_stdio(0); cin.tie(0), cout.tie(0); // 加速cin, cout
+    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
     cin >> T;
-    while(T-- > 0) {
-        cin >> n;
-        memset(cnt0, 0, sizeof(cnt0)), memset(cnt1, 0, sizeof(cnt1));
-        for(int i = 1; i <= n; i++) {
-            cin >> nums[i];
-            for(int j = 0; j < 32; j++) {
-                if((1 << j) & nums[i]) cnt1[j]++;
-                else cnt0[j]++;
-            }
-        }
-        ll ans = 0;
-        for(int i = 1; i <= n; i++)
-            ans = max(ans, cal(nums[i]));
-        cout << ans << "\n";
+    while (T--) {
+
     }
+    return 0;
 }
